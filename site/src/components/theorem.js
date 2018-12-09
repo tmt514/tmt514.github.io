88d5f786e7211746b6756b78b9c376adf75dd7ac
tmt514 <math.tmt514@gmail.com>
2018-12-09 11:57:58 -0500

    xd

--- site/src/components/theorem.js
+++ site/src/components/theorem.js
@@ -2,15 +2,22 @@ import React, {Component} from 'react';
 
 class Theorem extends Component {
     render() {
+
         return (
         <div className={`message ${this.props.c||"is-warning"}`}>
-        <div className="message-header">
-        {this.props.title}
-        </div>
+        {
+            this.props.title !== undefined
+            && (
+                <div className="message-header">
+                {this.props.title}
+                </div>
+            )
+        }
         <div className="message-body">
         {this.props.children}
         </div></div>
         )
+        
     }
 };
 
---
+++ site/src/pages/cp/codechef/snckel19.md
@@ -0,0 +1,19 @@
+---
+category: "cp"
+path: "/cp/snckel19"
+date: "2018-12-08"
+title: "SnackDown19 Elimination Round"
+link: "https://www.codechef.com/SNCKEL19"
+---
+
+唉唉，今天狀況超級不對啊。整個就是撞牆卡在 Suffix Palindrome，到了快三個辦鐘頭才想到 Lighting Rectangle 要怎麼寫，還錯了一次，真是奇慘無比。
+
+## Lighting Rectangle [RECTLIT](https://www.codechef.com/SNCKEL19/problems/RECTLIT)
+
+<theorem c="is-info">
+在一個二維座標平面上有一個 $(0, 0)$ 到 $(N-1, N-1)$ 的正方形。在這個正方形區域內有 $K$ 盞燈。對於每一盞燈而言，這盞燈為原點可以把整個平面分成四個象限。而你可以為每一盞燈選擇照亮其中一個象限（在邊界上也算是有被照亮）。現在給你這 $K$ 盞燈的位置，是否存在一種方法，讓它們可以照亮整個正方形的範圍呢？
+</theorem>
+
+### 我的解法
+
+我們可以d
